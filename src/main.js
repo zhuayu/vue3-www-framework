e09dc647540ffbb44d9c56392a8ad6ddca50b78e
Jax <294242589@qq.com>
2024-02-03 14:08:36 +0800

    fix: add element ui style

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import router from './router'
 
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
